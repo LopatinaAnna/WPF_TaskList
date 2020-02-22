e089fce17a399c466611a9896d969d43acb44072
Анна Лопатина <an.lopatina@mail.ru>
2020-02-22 23:59:42 +0200

    Деление на модули

--- WPF_TaskList/MainWindow.xaml.cs
+++ WPF_TaskList/MainWindow.xaml.cs
@@ -1,61 +1,14 @@
-﻿using System;
-using System.ComponentModel;
-using System.Windows;
-using WPF_TaskList.Models;
-using WPF_TaskList.Services;
+﻿using System.Windows;
+using WPF_TaskList.ViewModels;
 
 namespace WPF_TaskList
 {
     public partial class MainWindow : Window
     {
-        private BindingList<TaskModel> _taskListData;
-
-        private FileIOService _fileIOService;
-
-        private readonly string PATH = $"{Environment.CurrentDirectory}\\taskListData.json";
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            _fileIOService = new FileIOService(PATH);
-
-            try
-            {
-                _taskListData = _fileIOService.LoadData();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                Close();
-            }
-
-            //_taskListData = new BindingList<TaskModel>()
-            //{
-            //    new TaskModel() {TaskText = "test1"},
-            //    new TaskModel() {TaskText = "test2", IsChecked = true}
-            //};
-           
-            dgTaskList.ItemsSource = _taskListData;
-            _taskListData.ListChanged += _taskListData_ListChanged;
-        }
-
-        private void _taskListData_ListChanged(object sender, ListChangedEventArgs e)
-        {
-            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
-            {
-                try
-                {
-                    _fileIOService.SaveData(sender);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    Close();
-                }
-            }
+            DataContext = new ViewModel();
         }
     }
 }
---
+++ WPF_TaskList/ViewModels/ViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using WPF_TaskList.Models;
+using WPF_TaskList.Services;
+
+namespace WPF_TaskList.ViewModels
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private BindingList<TaskModel> _taskListData;
+        public BindingList<TaskModel> TaskListData
+        {
+            get { return _taskListData; }
+            set
+            {
+                _taskListData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private FileIOService _fileIOService;
+
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\taskListData.json";
+
+        public ViewModel()
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _taskListData = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+            _taskListData.ListChanged += _taskListData_ListChanged;
+        }
+
+        private void _taskListData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
